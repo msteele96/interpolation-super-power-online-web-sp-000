9c3ee10160d9b525b9a95e60f7ecea513f138438
Marc Steele <marcsteele2014@gmail.com>
2020-09-23 18:00:14 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-rainbow = [R:red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet]
+#rainbow = [R:red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet]
 
 def display_rainbow
   puts [rainbow]
